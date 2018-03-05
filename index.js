d127a977b4add6bab5bfdd422206468c6026e8f4
Modou Lo <modoulo99@icloud.com>
2018-03-05 09:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value })
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object
+}
+
