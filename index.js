d777e08244175cb7ab61b216ad967fabad0e4cf0
Modou Lo <modoulo99@icloud.com>
2018-03-05 09:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
   return object
 }
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
+  delete newObj.key
   return newObj
 }
 
