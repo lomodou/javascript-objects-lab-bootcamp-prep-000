b83d20d8bb2ab42c14bed6cd061e8bcca1c0ea7c
Modou Lo <modoulo99@icloud.com>
2018-03-05 09:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+  return Object.assign({}, object, {[key]: value })
 }
+
